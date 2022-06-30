9b7e99f2782aa19b7406a66ed8ae7cda13d1b0c3
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2022-06-30 23:07:29 +0200

    simple working gql

--- server/server.js
+++ server/server.js
@@ -7,21 +7,44 @@ const port = process.env.PORT || 4000;
 const typeDefs = gql`
   type Query {
     hello: String
-    status: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello, world!123",
-    status: () => "Maintenance",
+    notes: () => notes,
+    note: (args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: "Luege",
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
   },
 };
 
 let notes = [
-  { id: "1", content: "note 1" },
-  { id: "2", content: "note 2" },
-  { id: "3", content: "note 3" },
+  { id: "1", content: "note 1", author: "Ver" },
+  { id: "2", content: "note 2", author: "Mike" },
+  { id: "3", content: "note 3", author: "Elvis" },
 ];
 
 const server = new ApolloServer({
