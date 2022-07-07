ad96615647ec04769e8fb2a1d7b2e159e4796cc9
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2022-07-07 17:35:15 +0200

    auth - first step

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
+    email: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Note = mongoose.model("Note", UserSchema);
+
+module.exports = Note;
--- server/schema.js
+++ server/schema.js
@@ -14,9 +14,18 @@ module.exports = gql`
     createdAt: DateTime!
     updatedAt: DateTime!
   }
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
+  }
   type Mutation {
     newNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
